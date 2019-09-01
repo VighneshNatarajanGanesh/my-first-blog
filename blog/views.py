eb209076461cf9ba2ab46b842b5e7acca2da016c
Vighnesh Natarajan Ganesh <f20190131@pilani.bits-pilani.ac.in>
2019-09-02 02:36:42 +0530

    404 page

--- blog/views.py
+++ blog/views.py
@@ -11,8 +11,11 @@ def post_list(request):
     return render(request, 'blog/post_list.html',{'posts': posts})
 
 def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    if(pk>3):
+        return render(request, 'blog/pagenotfound.html')
+    else:
+        post = get_object_or_404(Post, pk=pk)
+        return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
     if request.method == "POST":
