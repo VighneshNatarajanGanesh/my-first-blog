08aef709ae998357a7815983450489d6f9c545d4
Vighnesh Natarajan Ganesh <f20190131@pilani.bits-pilani.ac.in>
2019-09-01 23:10:52 +0530

    Added view and template for detailed blog post as well as CSS for the site. settings.py debug is true again

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '18ts_!n11$w%a!kt=m8dttv+0tdv@j@$o!q#-gl2z+ac=p__84'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
